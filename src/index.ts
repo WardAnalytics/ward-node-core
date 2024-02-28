418bd7127a4b21204a0e1891870dcb6225427a5c
Gabriel Queiroz <gqmv@cin.ufpe.br>
2024-02-28 19:06:58 -0300

    Update package version and add bitcoinQuicknodeRequestExecutor

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./models/bitcoin/transaction";
+export * from "./quicknode/bitcoinQuicknodeRequestExecutor";
 export * from "./quicknode/quicknodeRequestExecutor";
 export * from "./utils/batchSizeManager";
 export * from "./utils/database";
