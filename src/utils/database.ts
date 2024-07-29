d0137bf63b33c2c238ea8056e5eabe1a86c261e9
Gabriel Vieira <gabriel.qmv@icloud.com>
2024-07-29 10:34:40 -0300

    chore: Update package.json version to 1.0.12 and add pg-connection-string and ts-postgres dependencies

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,47 +1,40 @@
-import sqlite3 from "sqlite3";
-
-interface MaxBlocksRow {
-  "MAX(block)": number | null;
-}
-
-interface MinBlocksRow {
-  "MIN(block)": number | null;
-}
+import { parse } from "pg-connection-string";
+import { connect } from "ts-postgres";
 
 export class UploadingRepository {
   dbPath: string;
+  connParams: any;
 
   constructor(dbPath: string) {
     this.dbPath = dbPath;
+    this.connParams = parse(dbPath);
   }
 
-  async getConn(): Promise<sqlite3.Database> {
-    return new sqlite3.Database(this.dbPath);
-  }
-
-  async getLatestBlockNumber(): Promise<number | null> {
-    const conn = await this.getConn();
-    return new Promise((resolve, reject) => {
-      conn.get("SELECT MAX(block) FROM blocks", (err, row: MaxBlocksRow) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(row["MAX(block)"]);
-        }
-      });
+  async getConn() {
+    return await connect({
+      host: this.connParams.host,
+      port: this.connParams.port,
+      user: this.connParams.user,
+      password: this.connParams.password,
+      database: this.connParams.database,
     });
+  }
+
+  async getLatestBlockNumber() {
+    const conn = await this.getConn();
+
+    const maxBlock = await conn
+      .query<number>("SELECT MAX(block) FROM blocks")
+      .one();
+    return maxBlock;
   }
 
   async getOldestBlockNumber(): Promise<number | null> {
     const conn = await this.getConn();
-    return new Promise((resolve, reject) => {
-      conn.get("SELECT MIN(block) FROM blocks", (err, row: MinBlocksRow) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(row["MIN(block)"]);
-        }
-      });
-    });
+
+    const minBlock = await conn
+      .query<number>("SELECT MIN(block) FROM blocks")
+      .one();
+    return minBlock;
   }
 }
